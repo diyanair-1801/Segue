f9cd538fb3810811bb899e29c638c8574a6f6356
Diya <diyanair.1801@gmail.com>
2022-07-19 11:48:29 -0500

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Diya Nair on 14/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
